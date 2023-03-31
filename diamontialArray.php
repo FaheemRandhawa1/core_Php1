2a15764c05c927f77a02f86bc1875a02e349c4d1
Faheem <faheemrahman14@gmail.com>
2023-03-31 10:24:53 +0500

    Update diamontialArray.php
    gh

--- diamontialArray.php
+++ diamontialArray.php
@@ -1,37 +1,13 @@
 <?php
 
 
-  
-$languages = array();
-  
-$languages['Python'] = array(
-    "first_release" => "1991", 
-    "latest_release" => "3.8.0", 
-    "designed_by" => "Guido van Rossum",
-    "description" => array(
-        "extension" => ".py", 
-        "typing_discipline" => "Duck, dynamic, gradual",
-        "license" => "Python Software Foundation License"
-    )
-);
-  
-foreach ($languages as $key => $value) {
-    echo $key . "\n";
-    foreach ($value as $sub_key => $sub_val) {
-                  
-        // If sub_val is an array then again
-        // iterate through each element of it
-        // else simply print the value of sub_key
-        // and sub_val
-        if (is_array($sub_val)) {
-            echo $sub_key . " : \n";
-            foreach ($sub_val as $k => $v) {
-                echo "\t" .$k . " = " . $v . "\n";
-            }
-        } else {
-            echo $sub_key . " = " . $sub_val . "\n";
-        }
-    }
-}
+$movie[0]="Shaolin Monk";
+$movie[1]="Drunken Master";
+$movie[2]="American Ninja";
+$movie[3]="Once upon a time in China";
+$movie[4]="Replacement Killers";
+echo $movie[3];
+$movie[3] = " Eastern Condors";
+echo $movie[3];
   
 ?>
